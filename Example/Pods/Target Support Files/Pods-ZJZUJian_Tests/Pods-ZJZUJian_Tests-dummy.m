c826731253aa5403413b35c3fbd70c2d06715f72
chenzhijian <czzj0212@163.com>
2020-12-30 21:39:43 +0800

    delete

--- Example/Pods/Target Support Files/Pods-ZJZUJian_Tests/Pods-ZJZUJian_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ZJZUJian_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_ZJZUJian_Tests
-@end
--- Example/Pods/Target Support Files/ZJZUJian/ZJZUJian-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ZJZUJian : NSObject
-@end
-@implementation PodsDummy_ZJZUJian
-@end
