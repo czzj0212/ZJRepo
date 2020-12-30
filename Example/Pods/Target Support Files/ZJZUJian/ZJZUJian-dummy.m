9e3a78845be62d2bf75ec4df86176459153c9746
chenzhijian <czzj0212@163.com>
2020-12-30 20:59:42 +0800

    pod

---
+++ Example/Pods/Target Support Files/ZJZUJian/ZJZUJian-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZJZUJian : NSObject
+@end
+@implementation PodsDummy_ZJZUJian
+@end
---
+++ ZJZUJian/Classes/ZJZUJianOBJ.h
@@ -0,0 +1,17 @@
+//
+//  ZJZUJianOBJ.h
+//  Pods-ZJZUJian_Example
+//
+//  Created by Maybe_ on 2020/12/30.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZJZUJianOBJ : NSObject
+
++(void)sayByebye ;
+@end
+
+NS_ASSUME_NONNULL_END
